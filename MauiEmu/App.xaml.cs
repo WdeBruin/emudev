2fe0c51240b85561be51b778f0513cb5ef60cf69
Bruin, Wouter de <wouter.de.bruin@capgemini.com>
2022-09-25 00:48:21 +0200

    Moved everything to a maui project. Draw is broken, cannot find mainthread. First clear screen instruction not correctly parsed. Rom is loaded.

---
+++ MauiEmu/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiEmu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiEmu/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiEmu
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
